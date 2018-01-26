5223478e997aa561ab4aaac4cec2dd460aa3d11b
justsully0 <sulman7@gmail.com>
2018-01-26 10:18:49 -0500

    Made a general window, with hello world as the window name.

--- src/VirtualCasino.java
+++ src/VirtualCasino.java
@@ -1,5 +1,19 @@
-public class VirtualCasino {
-	public static void main(String args[]){
-		System.out.print("Hello Wolrd");
-	}
+import javax.swing.*; 
+
+import java.awt.*; 
+
+import java.awt.event.*; 
+
+public class VirtualCasino extends JFrame {
+		VirtualCasino(String title) { 
+        this.setSize(500,500); 
+        setTitle(title); 
+    }
+
+    public static void main(String[] args) {
+    	VirtualCasino window = new VirtualCasino("Hello World");
+    	window.setVisible(true);
+    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
 }
+
