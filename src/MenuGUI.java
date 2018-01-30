a928600db0227e3bbdb6a8e69e5aac1a8ca92355
Marc Chesebro <marc.chesebro@gmail.com>
2018-01-30 16:22:27 -0500

    Added other buttons and a title and made the layout horizontal

--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -8,12 +8,21 @@ public class MenuGUI extends JFrame {
 
         // Create JPanel
         JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        // Create Jlabels
+        JLabel header = new JLabel("Virtual Casino");
 
         // Create Jbuttons
-        JButton button = new JButton("Slots");
+        JButton slotButton = new JButton("Slots");
+        JButton jackButton = new JButton("Black Jack");
+        JButton roulButton = new JButton("Roulette");
 
-        // Add button to panel
-        panel.add(button);
+        // Add buttons and labels to the panel
+        panel.add(header);
+        panel.add(slotButton);
+        panel.add(jackButton);
+        panel.add(roulButton);
 
         // Add panel to frame
         this.getContentPane().add(panel);
