70eb4f27e5f5bf638ea51b457ab62a287887b935
Marc Chesebro <marc.chesebro@gmail.com>
2018-02-01 13:49:10 -0500

    Added player class and made virtual casino use it

--- src/VirtualCasino.java
+++ src/VirtualCasino.java
@@ -7,9 +7,27 @@ import java.awt.event.*;
 public class VirtualCasino {
 
     public static void main(String[] args) {
+    	// create player with $100
+    	Player player = new Player(100);
+
     	MenuGUI window = new MenuGUI("Virtual Casino");
     	window.setVisible(true);
     	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    	//Have buttons call
+		//BlackJack.play(player);
     }
+
+    public static class Blackjack {
+
+    	// have buttons call this with the player
+    	public static void play(Player player){
+
+    		do{
+    			//play the game
+			}while(true);
+
+		}
+	}
 }
 
