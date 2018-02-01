583773ed198c217cfd96f30057595033628244e3
Marc Chesebro <marc.chesebro@gmail.com>
2018-02-01 13:49:55 -0500

    added helper functions

--- src/Card.java
+++ src/Card.java
@@ -14,12 +14,13 @@ public class Card {
         if(this.number > 1 && this.number < 11){
             return new int[]{this.number};
 
-            // Face cards(over 10) have a value of 10
+        // Face cards(over 10) have a value of 10
         }else if(this.number > 10){
             return new int[]{10};
 
-            // Aces have the value of wither 1 or 11
+        // Aces have the value of wither 1 or 11
         }else{
+
             return new int[]{1, 11};
         }
     }
