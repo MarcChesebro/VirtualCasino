2caa0268d47a74b8707959664fd9660779a85631
brendancaywood <brendan.m.caywood@gmail.com>
2018-02-01 16:09:45 -0500

    Fixing Merge Conflict

--- src/VirtualCasino.java
+++ src/VirtualCasino.java
@@ -7,19 +7,12 @@ import java.awt.event.*;
 public class VirtualCasino {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
- 
-=======
     	// create player with $100
     	Player player = new Player(100);
 
-    	MenuGUI window = new MenuGUI("Virtual Casino");
-    	window.setVisible(true);
-    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     	//Have buttons call
 		//BlackJack.play(player);
->>>>>>> f0ffbd19324761f5058b7baee3aa918c0360d5e8
     }
 
     public static class Blackjack {
