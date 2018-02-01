f0ffbd19324761f5058b7baee3aa918c0360d5e8
Marc Chesebro <marc.chesebro@gmail.com>
2018-02-01 14:08:47 -0500

    added classes for all the games

--- src/VirtualCasino.java
+++ src/VirtualCasino.java
@@ -20,11 +20,33 @@ public class VirtualCasino {
 
     public static class Blackjack {
 
-    	// have buttons call this with the player
-    	public static void play(Player player){
+		// have buttons call this with the player
+		public static void play(Player player){
 
-    		do{
-    			//play the game
+			do{
+				//play the game
+			}while(true);
+
+		}
+	}
+	public static class Slots {
+
+		// have buttons call this with the player
+		public static void play(Player player){
+
+			do{
+				//play the game
+			}while(true);
+
+		}
+	}
+	public static class Roulette {
+
+		// have buttons call this with the player
+		public static void play(Player player){
+
+			do{
+				//play the game
 			}while(true);
 
 		}
