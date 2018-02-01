7251db9a1704071751fb27dbbc6f78bbbc38aa37
Marc Chesebro <marc.chesebro@gmail.com>
2018-02-01 13:49:10 -0500

    Added player class and made virtual casino use it

---
+++ src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+    private int money;
+
+    public Player(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    //use this to add or remove money
+    public void changeMoney(int change){
+        this.money += change;
+    }
+}
