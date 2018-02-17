3f395a9f12d82d9111fccc72c4be82489858bbae
justsully0 <sulman7@gmail.com>
2018-02-17 16:28:43 -0500

    Added basic slots functionality

--- src/Slots.java
+++ src/Slots.java
@@ -4,38 +4,97 @@ import java.util.Random;
 
 public class Slots{
 
-	public static void play(Player play){
+	public static final int NUMSLOTS=3;
+
+	public static void play(Player player){
 
 		Scanner scanner = new Scanner(System.in);
 
-		do{
-			System.out.print("Place bet: ");
-			int bet = scanner.nextInt();
+		int options=1;
 
-			//check bet if bad, exit
+		while(options != 0){
 
-			int gains = pullLever(bet);
+			System.out.println("=======================");
+			System.out.println("Enter '1' to place bet.\nEnter '0' to quit.");
+			System.out.print("Input: ");
+			options=scanner.nextInt();
+			System.out.println("=======================");
 
-			player.changeMoney(gains);
+			//If the user wants to place a bet
+			if(options == 1){
 
-		}while(Player.getMoney()>0);
+				System.out.print("Place bet: ");
+				int bet = scanner.nextInt();
+
+				if(bet>player.getMoney()){
+					System.out.println("Insuficient funds. Bet invalid.\nCurrent balance: "+player.getMoney());
+					continue;
+				}
+
+				int gains = pullLever(bet);
+				gains = gains - bet;
+				player.changeMoney(gains);
+			}
+			else
+				return;
+		}
 		return;
 	}
 
-	public int oneSlot(){
+	public static int oneSlot(){
 		Random rand = new Random();
-
 		return (rand.nextInt(100)+1);
 	}
 
-	public int pullLever(int bet){
+	public static int pullLever(int bet){
 
-		//make array of slots
-		//initialize each element
-		//check the value
-		//if you win multiply the bet by the winnings and send (winnings - bet) back
-		//if you lose send the bet back negative
+		int indivSlots[] = new int[NUMSLOTS]; 
+		
+		int ace=0, king=0, queen=0, jack=0, ten = 0;
+		double winner=0.0;
+
+		for(int i = 0; i<NUMSLOTS; i++){
+			
+			int temp = oneSlot();
+			if(temp<40){
+				indivSlots[i] = 1;
+				ace++;
+			}else if(temp < 65 && temp >= 40){
+				indivSlots[i] = 2;
+				king++;
+			}else if(temp < 85 && temp >= 65){
+				indivSlots[i] = 3;
+				queen++;
+			}else if(temp < 95 && temp >= 85){
+				indivSlots[i] = 4;
+				jack++;
+			}else if(temp <= 100 && temp >= 95){
+				indivSlots[i] = 5;
+				ten++;
+			}
+			System.out.println("indivSlots: "+indivSlots[i]);
+		}
+
+		if(ace == NUMSLOTS)
+			winner = 5.0;
+		else if(king == NUMSLOTS)
+			winner = 1.5;	
+		else if(queen == NUMSLOTS)
+			winner = 1.2;	
+		else if(jack == NUMSLOTS)
+			winner = 1.1;	
+		else if(ten == NUMSLOTS)
+			winner = 1;	
+		else
+			winner = 0;
 
 
+
+		int retVal = bet * (int)winner;
+		
+		System.out.println("ret: "+retVal);
+
+
+		return retVal;
 	}
 }
--- src/VirtualCasino.java
+++ src/VirtualCasino.java
@@ -14,15 +14,27 @@ public class VirtualCasino {
 
     	//Have buttons call
     	System.out.println("Please choose a game:");
-    	System.out.println("'1' for roulette, '2' for blackjack, '3' for slots, '4' for player options.");
+    	System.out.println("'1' for roulette, '2' for blackjack, '3' for slots, '4' for player options, '0' to exit.");
     	System.out.print("Input: ");
     	int x = scanner.nextInt();
 		//Blackjack.play(player);
     	if(x == 1) {
     		Roulette.play(player);
-    	}
-    	else if(x == 3){
+    	}else if (x == 2){
+    		Blackjack.play(player);
+    	}else if(x == 3){
     		Slots.play(player);
+    	}else if(x == 4){
+    		System.out.println("enter '1' to add money, '0' to exit")
+    		System.out.print("Input: ");
+    		int playerOptionsInput = scanner.nextInt();
+    		if(playerOptionsInput == 1){
+    			System.out.print("how much?: ");
+    			int moreMoneyMoreProblems = scanner.nextInt();
+    			player.changeMoney(moreMoneyMoreProblems);
+    			System.out.println("New balance: "+player.getMoney());
+    		}
+
     	}
     }
 
