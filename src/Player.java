d558533fbd16bce76a74d548b657b27d2e0343ef
Marc Chesebro <marc.chesebro@gmail.com>
2018-02-23 16:41:13 -0500

    added javadoc comments

--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,43 @@
+/**
+ * Represents the player of the game.
+ *
+ * @author Marc Chesebro
+ * @version 2/23/2018
+ */
 public class Player {
 
+    /** current amount of money the player has. */
     private int money;
 
+    /**
+     * Constructor that take the amount of money the player starts with.
+     * @param int money amount of money the player starts with.
+     */
     public Player(int money) {
         this.money = money;
     }
 
+    /**
+     * get the amount of money the player currently has.
+     * @return the amount of money the player has as an int.
+     */
     public int getMoney() {
         return money;
     }
 
+    /**
+     * set the amount of money the player currently has.
+     * @param int money the amount of money to set the player to.
+     */
     public void setMoney(int money) {
         this.money = money;
     }
 
-    //use this to add or remove money
+    /**
+     * Function for changing the amount of money a player has.
+     * can accept negative numbers to remove Money
+     * @param int change amount to change.
+     */
     public void changeMoney(int change){
         this.money += change;
     }
