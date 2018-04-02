d74ab92d4d53f1a9fea3d67f09f76e98887b1410
brendancaywood <brendan.m.caywood@gmail.com>
2018-04-02 12:05:53 -0400

    gui setup Still have errors when hitting buttons

--- src/GuiController.java
+++ src/GuiController.java
@@ -6,12 +6,30 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Label;
-public class GuiController implements Initializable {
-	private boolean started = false;
+public class GuiController extends VirtualCasino implements Initializable {
+	@FXML Button refreshButton;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		started = false;
-		
 	}
-	
+	@FXML 
+	private void blackjackButton() {
+		blackjack();
+	}
+	@FXML 
+	private void slotButton() {
+		blackjack();
+	}
+	@FXML
+	private void refreshButton() {
+		start();
+	}
+	@FXML 
+	private void rouButton() {
+		blackjack();
+	}
+	@FXML 
+	private void quitMItem() {
+		System.exit(0);
+	}
 }
