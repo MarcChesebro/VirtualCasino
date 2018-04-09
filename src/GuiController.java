4b72d729530e02f73378bcecee5ad27e90b2d9f5
justsully0 <sulman7@gmail.com>
2018-04-08 20:31:27 -0400

    fixed comment issue

--- src/GuiController.java
+++ src/GuiController.java
@@ -1,35 +1,35 @@
-// import java.net.URL;
-// import java.util.ResourceBundle;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-// import javafx.fxml.FXML;
-// import javafx.fxml.Initializable;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.MenuItem;
-// import javafx.scene.control.Label;
-// public class GuiController extends VirtualCasino implements Initializable {
-// 	@FXML Button refreshButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Label;
+public class GuiController extends VirtualCasino implements Initializable {
+	@FXML Button refreshButton;
 	
-// 	@Override
-// 	public void initialize(URL arg0, ResourceBundle arg1) {
-// 	}
-// 	@FXML 
-// 	private void blackjackButton() {
-// 		blackjack();
-// 	}
-// 	@FXML 
-// 	private void slotButton() {
-// 		blackjack();
-// 	}
-// 	@FXML
-// 	private void refreshButton() {
-// 		start();
-// 	}
-// 	@FXML 
-// 	private void rouButton() {
-// 		blackjack();
-// 	}
-// 	@FXML 
-// 	private void quitMItem() {
-// 		System.exit(0);
-// 	}
-// }
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+	@FXML 
+	private void blackjackButton() {
+		blackjack();
+	}
+	@FXML 
+	private void slotButton() {
+		blackjack();
+	}
+	@FXML
+	private void refreshButton() {
+		start();
+	}
+	@FXML 
+	private void rouButton() {
+		blackjack();
+	}
+	@FXML 
+	private void quitMItem() {
+		System.exit(0);
+	}
+}
