9211b74b3b4929c8acc677ddf270e1c6480b607d
Marc Chesebro <marc.chesebro@gmail.com>
2018-04-14 15:41:44 -0400

    updated to use ScreenController

---
+++ src/BlackJackGUI.java
@@ -0,0 +1,14 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BlackJackGUI implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+}
--- src/MenuGUI.java
+++ src/MenuGUI.java
@@ -10,12 +10,18 @@ import static javafx.application.Application.launch;
 
 public class MenuGUI extends Application {
 	 public static VirtualCasino casino = new VirtualCasino();
+	 public static ScreenController screenController;
 	 
      @Override
      public void start(Stage primaryStage) throws Exception {
          try {
             Parent root = FXMLLoader.load(getClass().getResource("/gui_css.fxml"));
              Scene scene = new Scene(root);
+             screenController = new ScreenController(scene);
+
+             screenController.addScreen("main", FXMLLoader.load(getClass().getResource("/gui_css.fxml")));
+             screenController.addScreen("blackjack", FXMLLoader.load(getClass().getResource("/blackjack.fxml")));
+
              primaryStage.setScene(scene);
              primaryStage.setTitle("Virtual Casino");
              primaryStage.setResizable(false);
