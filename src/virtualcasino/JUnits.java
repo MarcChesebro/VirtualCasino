030dd5bbcafea05013825d462fe4ab4403125860
Marc Chesebro <marc.chesebro@gmail.com>
2018-04-20 10:30:33 -0400

    junit updated

--- src/virtualcasino/JUnits.java
+++ src/virtualcasino/JUnits.java
@@ -1,9 +1,9 @@
 package virtualcasino;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 
